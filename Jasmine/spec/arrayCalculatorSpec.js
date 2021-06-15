2169c547e0dd61341a37dd2e668fdaf624acfd54
“Codeboy3000” <iantfleming1992@gmail.com>
2021-06-15 13:25:31 +0100

    responds to sumLargestNums

--- Jasmine/spec/arrayCalculatorSpec.js
+++ Jasmine/spec/arrayCalculatorSpec.js
@@ -5,6 +5,11 @@ describe('ArrayCalculator', function(){
   beforeEach(function(){
     arrayCalculator = new ArrayCalculator();
   });
+
+  it('responds to the method sumLargestNums', function(){
+    expect(arrayCalculator.sumLargestNums([])).toBeDefined();
+  })
+
   it('an empty array returns 0', function(){
     expect(arrayCalculator.sumLargestNums([])).toEqual([0]);
   });
